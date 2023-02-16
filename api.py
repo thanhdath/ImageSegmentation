efa7ead856e3343d789052dfc981dc81640e2729
Thanh Dat <thanhdath97@gmail.com>
2023-02-16 11:10:07 +0700

    Add parameter 'max number of objects' to API get objects

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ import cv2
 
 from PIL import Image
 from ade import CLASSES
-from segmentation import get_largest_object_polygon, DeepLabModel
+from segmentation import get_top_k_objects_polygon, DeepLabModel
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -24,10 +24,16 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 @app.route("/get_main_object", methods=["POST"])
 def get_main_object():
     file = request.files["image"]
+    max_n_objects = int(request.form.get("max_n_objects", 1))
     x1 = float(request.form.get("x1"))
+    print(x1)
     y1 = float(request.form.get("y1"))
+    print(y1)
     x2 = float(request.form.get("x2"))
+    print(x2)
     y2 = float(request.form.get("y2"))
+    print(y2)
+    print(file)
 
     original_filename = file.filename
     original_ext = original_filename.split(".")[-1]
@@ -38,34 +44,43 @@ def get_main_object():
     file.save(temp_filepath)
 
     img = Image.open(temp_filepath)
+    print("Size: ", img.size)
     img = img.crop((x1, y1, x2, y2))
-    resized_im, seg_map = MODEL.run(img)
+    _, seg_map = MODEL.run(img)
 
     filter_seg_map = np.zeros_like(seg_map, dtype=np.int32)
     for label in CONSIDER_CLASSES.keys():
         filter_seg_map[seg_map == ORI_CLASS2IDX[label]] = CONSIDER_CLASSES[label]
-
-    box = get_largest_object_polygon(
-        filter_seg_map, img.width, img.height, IDX2CONSIDER_CLASS
+    boxes = get_top_k_objects_polygon(
+        filter_seg_map,
+        x1,
+        y1,
+        img.width,
+        img.height,
+        IDX2CONSIDER_CLASS,
+        max_n_objects=max_n_objects,
     )
 
-    img_cv = np.asarray(img)
-    img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-    img_cv = cv2.polylines(
-        img_cv, [np.array(box["points"])], True, (255, 0, 0), thickness=2
-    )
-    cv2.imwrite(f"{TEMP_DIR}/{temp_filename}-box.png", img_cv)
-
+    if len(boxes) > 0:
+        img_cv = np.asarray(img)
+        img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+        for box in boxes:
+            img_cv = cv2.polylines(
+                img_cv, [np.array(box["points"])], True, (255, 0, 0), thickness=2
+            )
+            cv2.imwrite(f"{TEMP_DIR}/{temp_filename}-box.png", img_cv)
+    else:
+        print("len(points)==0")
     os.remove(temp_filepath)
 
-    return jsonify(box)
+    return jsonify(boxes)
 
 
 if __name__ == "__main__":
     os.makedirs(TEMP_DIR, exist_ok=True)
 
     MODEL = DeepLabModel(
-       "deeplabv3_xception_ade20k_train/frozen_inference_graph.pb"
+        "deeplabv3_xception_ade20k_train/frozen_inference_graph.pb"
         # "deeplabv3_mnv2_ade20k_train_2018_12_03/frozen_inference_graph.pb"
     )
     print("model loaded successfully!")
@@ -78,10 +93,9 @@ if __name__ == "__main__":
         "skyscraper": 1,
         "car, auto, automobile, machine, motorcar": 2,
         "truck, motortruck": 2,
-        "airplane, aeroplane, plane": 3
+        "airplane, aeroplane, plane": 3,
     }  # class to our new label indices
 
     IDX2CONSIDER_CLASS = {1: "building", 2: "car+truck", 3: "plane"}
 
     app.run()
- 
--- segmentation.py
+++ segmentation.py
@@ -117,12 +117,17 @@ def label_to_color_image(label):
     return colormap[label]
 
 
-def get_largest_object_polygon(segment_img, im_width, im_height, IDX2CLASS):
+def get_top_k_objects_polygon(
+    segment_img, origin_x, origin_y, im_width, im_height, IDX2CLASS, max_n_objects=1
+):
     vals = list(set(segment_img.flatten()))
     vals = [x for x in vals if x != 0]
-
     if len(vals) == 0:
-        return None
+        return {
+            "label": "",
+            "shape_type": "",
+            "points": [],
+        }
 
     # find largest object
 
@@ -147,38 +152,44 @@ def get_largest_object_polygon(segment_img, im_width, im_height, IDX2CLASS):
         )
         # contours = [x for x in contours if cv2.contourArea(x) > 20]
         contours_areas = [cv2.contourArea(x) for x in contours]
-
         all_contours.extend(contours)
         all_contours_areas.extend(contours_areas)
         all_labels.extend([val] * len(contours))
 
-    largest_ind = np.argmax(all_contours_areas)
-    contour = all_contours[largest_ind]
+    # sort objects by areas (number of pixels of each object)
+    sorted_inds = np.argsort(all_contours_areas)[::-1][:max_n_objects]
+    selected_contours = [all_contours[i] for i in sorted_inds]
+    selected_labels = [all_labels[i] for i in sorted_inds]
 
-    # smooth contour
-    contour = contour.reshape(-1, 2)
-    new_contour = [contour[0]]
-    for i in range(1, len(contour)):
-        prev_pt = new_contour[-1]
-        dist = np.sqrt(np.sum((contour[i] - prev_pt) ** 2))
-        if dist > 10:
-            new_contour.append(contour[i])
-    if len(new_contour) >= 3:
-        contour = new_contour
+    # Iterate over contours, smooth and draw polygons
+    all_boxes = []
+    for contour, label in zip(selected_contours, selected_labels):
+        # smooth contour
+        contour = contour.reshape(-1, 2)
+        new_contour = [contour[0]]
+        for i in range(1, len(contour)):
+            prev_pt = new_contour[-1]
+            dist = np.sqrt(np.sum((contour[i] - prev_pt) ** 2))
+            if dist > 10:
+                new_contour.append(contour[i])
+        if len(new_contour) >= 3:
+            # filter too small objects which is often due to noisy
+            contour = new_contour
 
-    box = {
-        "label": IDX2CLASS[all_labels[largest_ind]],
-        "shape_type": "polygon",
-        "points": [
-            [
-                int(x * im_width / mask.shape[1]),
-                int(y * im_height / mask.shape[0]),
-            ]
-            for x, y in contour
-        ],
-    }
+        box = {
+            "label": IDX2CLASS[label],
+            "shape_type": "polygon",
+            "points": [
+                [
+                    int(origin_x + int(x * im_width / mask.shape[1])),
+                    int(origin_y + int(y * im_height / mask.shape[0])),
+                ]
+                for x, y in contour
+            ],
+        }
+        all_boxes.append(box)
 
-    return box
+    return all_boxes
 
 
 if __name__ == "__main__":
